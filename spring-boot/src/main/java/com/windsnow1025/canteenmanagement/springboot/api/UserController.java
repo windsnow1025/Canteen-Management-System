eae916db5c6c3147ea2d66a49b31f98dce60c948
windsnow1025 <windsnow1025@gmail.com>
2023-12-10 05:18:19 +0000

    spring structured

---
+++ spring-boot/src/main/java/com/windsnow1025/canteenmanagement/springboot/api/UserController.java
@@ -0,0 +1,66 @@
+package com.windsnow1025.canteenmanagement.springboot.api;
+
+import com.windsnow1025.canteenmanagement.springboot.db.JDBCHelper;
+import com.windsnow1025.canteenmanagement.springboot.db.UserDAO;
+import com.windsnow1025.canteenmanagement.springboot.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserDAO userDAO;
+
+    public UserController() {
+        JDBCHelper jdbcHelper = new JDBCHelper();
+        this.userDAO = new UserDAO();
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<User> getUser(@RequestParam Map<String, String> request) {
+        try {
+            User user = new User();
+            if (user != null) {
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<User> loginUser(@RequestBody Map<String, String> request) {
+        try {
+            User user = new User();
+            if (user != null) {
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Map<String, Object>> signupUser(@RequestBody Map<String, String> request) {
+        try {
+            boolean isSignedUp = true;
+            if (isSignedUp) {
+                return ResponseEntity.ok(Map.of("status", "Success", "message", "Signup successful"));
+            } else {
+                return ResponseEntity.badRequest().body(Map.of("status", "Failure", "message", "Signup failed"));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(Map.of("status", "Error", "message", e.getMessage()));
+        }
+    }
+
+
+}
