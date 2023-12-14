a093bfebc2975984162677b09249ef4ee1c8f2a1
windsnow1025 <windsnow1025@gmail.com>
2023-12-14 05:53:24 +0000

    feat: update user api path

--- spring-boot/src/main/java/com/windsnow1025/canteenmanagement/springboot/api/UserController.java
+++ spring-boot/src/main/java/com/windsnow1025/canteenmanagement/springboot/api/UserController.java
@@ -1,6 +1,5 @@
 package com.windsnow1025.canteenmanagement.springboot.api;
 
-import com.windsnow1025.canteenmanagement.springboot.dao.UserDAO;
 import com.windsnow1025.canteenmanagement.springboot.logic.UserLogic;
 import com.windsnow1025.canteenmanagement.springboot.model.User;
 import org.slf4j.Logger;
@@ -15,13 +14,11 @@ import java.util.Map;
 @RequestMapping("/user")
 public class UserController {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserDAO.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserLogic userLogic;
-    private final UserDAO userDAO;
 
     public UserController() {
         this.userLogic = new UserLogic();
-        userDAO = new UserDAO();
     }
 
     @GetMapping("/info")
@@ -77,7 +74,7 @@ public class UserController {
         }
     }
 
-    @PostMapping("/updatePassword")
+    @PostMapping("/update/password")
     public ResponseEntity<Map<String, Object>> updatePassword(@RequestHeader("Authorization") String token, @RequestBody Map<String, String> request) {
         try {
             String newPassword = request.get("password");
@@ -93,7 +90,7 @@ public class UserController {
         }
     }
 
-    @PostMapping("/updateType")
+    @PostMapping("/update/type")
     public ResponseEntity<Map<String, Object>> updateType(@RequestHeader("Authorization") String token, @RequestBody Map<String, String> request) {
         try {
             String userType = request.get("userType");
@@ -110,7 +107,7 @@ public class UserController {
         }
     }
 
-    @PostMapping("/updateLevel")
+    @PostMapping("/update/level")
     public ResponseEntity<Map<String, Object>> updateLevel(@RequestHeader("Authorization") String token, @RequestBody Map<String, String> request) {
         try {
             String userLevel = request.get("userLevel");
