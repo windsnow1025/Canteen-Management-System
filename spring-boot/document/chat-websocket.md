c7de71014ce5e5039478e664d77607d4e29275f1
windsnow1025 <windsnow1025@gmail.com>
2023-12-21 11:03:13 +0000

    feat: chatroom websocket finished

---
+++ spring-boot/document/chat-websocket.md
@@ -0,0 +1,12 @@
+# Chatroom WebSocket
+
+## ws://\<host\>:8082/chatroom
+
+Message:
+```json
+{
+  "sender": "master",
+  "receiver": "admin",
+  "content": "Hey admin, I'm master!"
+}
+```
--- spring-boot/src/main/java/com/windsnow1025/canteenmanagement/springboot/ChatWebSocketHandler.java
+++ spring-boot/src/main/java/com/windsnow1025/canteenmanagement/springboot/ChatWebSocketHandler.java
@@ -1,13 +1,35 @@
 package com.windsnow1025.canteenmanagement.springboot;
 
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+    private final Set<WebSocketSession> sessions = Collections.newSetFromMap(new ConcurrentHashMap<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+    }
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        session.sendMessage(message);
+        // Broadcast the received message to all connected clients
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(message);
+            }
+        }
     }
 }
