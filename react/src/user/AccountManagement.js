42efdfd767b2b1abfe60b4f5885cb0194c507551
fx1181855890 <1181855890@qq.com>
2023-12-22 14:55:22 +0800

    账户管理

--- react/src/user/AccountManagement.js
+++ react/src/user/AccountManagement.js
@@ -9,6 +9,7 @@ const { Panel } = Collapse;
 const AccountManagement = () => {
     const { userId } = useParams();
     const [users, setUsers] = useState([]);
+    const [newUserId, setNewUserId] = useState('');
     const [newUserType, setNewUserType] = useState('');
     const [newCanteenId, setNewCanteenId] = useState('');
     const [deleteUserId, setDeleteUserId] = useState('');
@@ -28,8 +29,9 @@ const AccountManagement = () => {
 
     const handleUpdateUserType = async () => {
         try {
-            await UserApi.updateUserType(userId, newUserType, newCanteenId);
+            await UserApi.updateUserType(newUserId, newUserType, newCanteenId);
             fetchUsers();
+            alert("更新成功");
         } catch (error) {
             console.error('Error updating user type:', error);
         }
@@ -39,6 +41,7 @@ const AccountManagement = () => {
         try {
             await UserApi.deleteUserById(deleteUserId);
             fetchUsers();
+            alert("删除成功");
         } catch (error) {
             console.error('Error deleting user:', error);
         }
@@ -53,6 +56,31 @@ const AccountManagement = () => {
                         <h1 className="mb-4 text-xl text-center">账号管理</h1>
 
                         <Collapse accordion>
+                            {/* 已有用户列表 */}
+                            <Panel header="已有用户列表" key="1">
+                                <div className="overflow-y-auto max-h-60">
+                                <ul>
+                                    {users.map((user) => (
+                                        <li key={user.id} className="flex justify-between items-center">
+                                            <span>
+                                                <div className="bg-white hover:bg-blue-dark text-black font-bold py-2 px-4 rounded">
+                                                    {/* 显示用户ID、用户名和用户类型 */}
+                                                    <p>ID: {user.id}</p>
+                                                    <p>Name: {user.username}</p>
+                                                    <p>Type: {user.userType}</p>
+                                                </div>
+                                            </span>
+                                            <button
+                                                className="bg-red-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded whitespace-nowrap"
+                                                onClick={() => handleDeleteUser(user.id)}
+                                            >
+                                                删除
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                                </div>
+                            </Panel>
                             {/* 更新用户类型 */}
                             <Panel header="更新用户类型" key="2">
                                 {/* 用户ID */}
@@ -60,18 +88,20 @@ const AccountManagement = () => {
                                 <input
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
                                     type="text"
-                                    value={userId}
-                                    readOnly
+                                    value={newUserId}
+                                    onChange={(e) => setNewUserId(e.target.value)}
                                 />
 
                                 {/* 用户类型 */}
                                 <p>用户类型:</p>
-                                <input
+                                <select
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                                    type="text"
                                     value={newUserType}
                                     onChange={(e) => setNewUserType(e.target.value)}
-                                />
+                                >
+                                    <option value="canteen_admin">餐厅管理员</option>
+                                    <option value="consumer">消费者</option>
+                                </select>
 
                                 {/* 餐厅ID */}
                                 <p>餐厅ID:</p>
