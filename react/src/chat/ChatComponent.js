a58c7dfec6c101be628a9abcccaa344300f3afcb
Admiraljj <89696583@qq.com>
2023-12-22 14:12:01 +0800

    modify chat ui

--- react/src/chat/ChatComponent.js
+++ react/src/chat/ChatComponent.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react';
 import useWebSocket from './useWebSocket';
 import NavBar from "../components/NavBar";
 import UserApi from "../api/UserApi";
-import CanteenApi from "../api/CanteenApi";
 const ChatComponent = () => {
   const [input, setInput] = useState('');
   const { messages, sendMessage, connected } = useWebSocket('ws://www.windsnow1025.com:8082/chatroom');
@@ -28,7 +27,6 @@ const ChatComponent = () => {
         };
 
         fetchUserName();
-        console.log(userName)
     }, []);
 
   return (
@@ -38,7 +36,8 @@ const ChatComponent = () => {
               <div className="p-8 bg-white rounded shadow-lg">
                   <h2 className="text-2xl font-bold mb-4">WebSocket Chat</h2>
                   <div className="mb-4">
-        <span className={`inline-block px-3 py-1 rounded-full text-sm font-semibold ${connected ? 'text-green-600 bg-green-200' : 'text-red-600 bg-red-200'}`}>
+        <span
+            className={`inline-block px-3 py-1 rounded-full text-sm font-semibold ${connected ? 'text-green-600 bg-green-200' : 'text-red-600 bg-red-200'}`}>
           {connected ? 'Connected' : 'Disconnected'}
         </span>
                   </div>
@@ -48,10 +47,11 @@ const ChatComponent = () => {
                       placeholder="Type a message..."
                       className="w-full p-2 mb-4 border rounded shadow-inner"
                   />
-                  <button onClick={handleSend} disabled={!connected} className="px-4 py-2 bg-blue-500 text-white rounded shadow hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed">
+                  <button onClick={handleSend} disabled={!connected}
+                          className="px-4 py-2 bg-blue-500 text-white rounded shadow hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed">
                       Send
                   </button>
-                  <div className="mt-8">
+                  <div className="mt-8 overflow-y-auto max-h-60">
                       <h3 className="text-xl font-semibold mb-2">Messages</h3>
                       {messages.map((message, index) => (
                           <div
