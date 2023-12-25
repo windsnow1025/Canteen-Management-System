a26b916ec691880ae2d3460a43eb3edc16026f8c
fx1181855890 <1181855890@qq.com>
2023-12-25 21:31:08 +0800

    Update ComplaintForm.js

--- react/src/user/ComplaintForm.js
+++ react/src/user/ComplaintForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ComplaintApi from '../api/ComplaintApi';
+import NavBar from "../components/NavBar";
 
 const ComplaintForm = () => {
     const [canteenId, setCanteenId] = useState('');
@@ -20,27 +21,37 @@ const ComplaintForm = () => {
     };
 
     return (
-        <div>
-            <h1>投诉表单</h1>
-            <label>
-                餐厅ID:
-                <input
-                    type="text"
-                    value={canteenId}
-                    onChange={(e) => setCanteenId(e.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                投诉详情:
-                <textarea
-                    value={detail}
-                    onChange={(e) => setDetail(e.target.value)}
-                />
-            </label>
-            <br />
-            <button onClick={handleAddComplaint}>提交投诉</button>
+        <>
+            <NavBar/>
+        <div className="flex items-center justify-center h-screen">
+            <div className="bg-white rounded-lg shadow-lg p-8 m-4 w-full max-w-md">
+                <h1 className="text-2xl font-bold mb-4">投诉表单</h1>
+                <label className="block mb-4">
+                    <span className="text-gray-700">餐厅ID:</span>
+                    <input
+                        type="text"
+                        value={canteenId}
+                        onChange={(e) => setCanteenId(e.target.value)}
+                        className="form-input mt-1 block w-full"
+                    />
+                </label>
+                <label className="block mb-4">
+                    <span className="text-gray-700">投诉详情:</span>
+                    <textarea
+                        value={detail}
+                        onChange={(e) => setDetail(e.target.value)}
+                        className="form-textarea mt-1 block w-full"
+                    />
+                </label>
+                <button
+                    onClick={handleAddComplaint}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    提交投诉
+                </button>
+            </div>
         </div>
+        </>
     );
 };
 
