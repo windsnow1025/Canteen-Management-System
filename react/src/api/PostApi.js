e4897a403f702a95bc9eeb4b7004a9ce6fc58fab
fx1181855890 <1181855890@qq.com>
2023-12-25 15:08:16 +0800

    add token to api.js

--- react/src/api/PostApi.js
+++ react/src/api/PostApi.js
@@ -2,41 +2,62 @@ import axios from 'axios';
 
 export default class PostApi {
     static async getPostInfos() {
-        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/infos`);
+        const token = localStorage.getItem('token');
+        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/infos`, {
+            headers: { Authorization: token ? `${token}` : '' }
+        });
         return res.data;
     }
 
     static async getPostInfoById(id) {
-        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/info/${id}`);
+        const token = localStorage.getItem('token');
+        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/info/${id}`, {
+            headers: { Authorization: token ? `${token}` : '' }
+        });
         return res.data;
     }
 
     static async getPostInfosByTitle(title) {
+        const token = localStorage.getItem('token');
         const res = await axios.post(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/infos/title`, {
             title: title
+        }, {
+            headers: { Authorization: token ? `${token}` : '' }
         });
         return res.data;
     }
 
     static async getPostInfosByUsername(username) {
+        const token = localStorage.getItem('token');
         const res = await axios.post(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/infos/username`, {
             username: username
+        }, {
+            headers: { Authorization: token ? `${token}` : '' }
         });
         return res.data;
     }
 
     static async createPost(postData) {
-        const res = await axios.post(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post`, postData);
+        const token = localStorage.getItem('token');
+        const res = await axios.post(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post`, postData, {
+            headers: { Authorization: token ? `${token}` : '' }
+        });
         return res.data;
     }
 
     static async likePost(id) {
-        const res = await axios.put(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/like/${id}`);
+        const token = localStorage.getItem('token');
+        const res = await axios.put(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/like/${id}`, null, {
+            headers: { Authorization: token ? `${token}` : '' }
+        });
         return res.data;
     }
 
     static async deletePost(id) {
-        const res = await axios.delete(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/${id}`);
+        const token = localStorage.getItem('token');
+        const res = await axios.delete(`${process.env.REACT_APP_HTTP_API_BASE_URL}/post/${id}`, {
+            headers: { Authorization: token ? `${token}` : '' }
+        });
         return res.data;
     }
 }
--- react/src/api/VoteApi.js
+++ react/src/api/VoteApi.js
@@ -1,45 +1,57 @@
 import axios from 'axios';
 
 export default class VoteApi {
-    // 获取所有投票信息
     static async getAllVoteInfos() {
-        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote/infos`);
+        const token = localStorage.getItem('token');
+        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote/infos`, {
+            headers: { Authorization: token ? `${token}` : '' }
+        });
         return res.data;
     }
 
-    // 获取特定食堂的投票信息 by Canteen ID
     static async getVoteInfosByCanteenId(canteenId) {
-        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote/infos/${canteenId}`);
+        const token = localStorage.getItem('token');
+        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote/infos/${canteenId}`, {
+            headers: { Authorization: token ? `${token}` : '' }
+        });
         return res.data;
     }
 
-    // 获取特定投票信息 by ID
     static async getVoteInfoById(voteId) {
-        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote/info/${voteId}`);
+        const token = localStorage.getItem('token');
+        const res = await axios.get(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote/info/${voteId}`, {
+            headers: { Authorization: token ? `${token}` : '' }
+        });
         return res.data;
     }
 
-    // 提交新的投票
     static async addVote(canteenId, title) {
+        const token = localStorage.getItem('token');
         const res = await axios.post(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote`, {
             canteenId: canteenId,
             title: title
+        }, {
+            headers: { Authorization: token ? `${token}` : '' }
         });
         return res.data;
     }
 
-    // 更新投票结果
     static async updateVoteResult(voteId, voteResult) {
+        const token = localStorage.getItem('token');
         const res = await axios.put(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote/vote-result`, {
             id: voteId,
             voteResult: voteResult
+        }, {
+            headers: { Authorization: token ? `${token}` : '' }
         });
         return res.data;
     }
 
-    // 删除投票信息 by ID
     static async deleteVoteById(voteId) {
-        const res = await axios.delete(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote/${voteId}`);
+        const token = localStorage.getItem('token');
+        const res = await axios.delete(`${process.env.REACT_APP_HTTP_API_BASE_URL}/vote/${voteId}`, {
+            headers: { Authorization: token ? `${token}` : '' }
+        });
         return res.data;
     }
 }
