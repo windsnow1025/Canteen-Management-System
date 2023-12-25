4077d26568b99e9bda6451234fcfddf7aa6bc1f8
fx1181855890 <1181855890@qq.com>
2023-12-25 20:10:39 +0800

    社区页

--- react/src/Community.js
+++ react/src/Community.js
@@ -1,21 +1,56 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Pagination } from 'antd';
 import { Input, Space } from 'antd';
 import NavBar from "./components/NavBar";
-import { content as PostContent } from "./content/PostContent";
+import PostApi from "./api/PostApi";
+import base64StringToDataURL from "./utils/Base64StringToDataURL";
 
 const { Search } = Input;
 const onSearch = (value, _e, info) => console.log(info?.source, value);
 const Community = () => {
+    const [postInfos, setPostInfos] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const pageSize = 5;
+
+    useEffect(() => {
+        // 获取帖子信息
+        const fetchPostInfos = async () => {
+            try {
+                const response = await PostApi.getPostInfos();
+                // 解析 Base64 图片字符串为 Data URL
+                const postsWithImages = await Promise.all(response.map(async (post) => {
+                    const imageUrl = await base64StringToDataURL(post.picture);
+
+                    return {
+                        ...post, picture: imageUrl,
+                    };
+                }));
+                setPostInfos(postsWithImages);
+            } catch (error) {
+                console.error('Error fetching evaluation infos:', error);
+            }
+        };
+
+        fetchPostInfos();
+    }, []);
 
     const onChange = (pageNumber) => {
         console.log('Page: ', pageNumber);
         setCurrentPage(pageNumber);
     };
+    const handleLikeClick = async (postId) => {
+        try {
+            // 调用 likePost 方法
+            const response = await PostApi.likePost(postId);
 
-    const currentPosts = PostContent.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+            // 处理成功的情况，例如刷新界面等
+            console.log(response);
+        } catch (error) {
+            // 处理错误，比如打印错误日志或者显示错误消息
+            console.error('Error liking post:', error);
+        }
+    };
+
 
     return (
         <>
@@ -24,8 +59,7 @@ const Community = () => {
             <Search className="w-1/3 mx-32" placeholder="输入搜索条件（帖子标题、内容、用户名）" onSearch={onSearch} enterButton />
             <div >
                 <div className="flex flex-wrap items-start justify-start bg-white rounded-lg shadow-lg mx-32 mt-4">
-                    {currentPosts.map((post, index) => (
-                        <a href="/PostContentPage">
+                    {postInfos.map((post, index) => (
                             <div key={index} className="flex flex-col space-y-4 mb-5 m-4 w-full p-4">
                                 <h2 className="text-2xl font-bold">{post.title}</h2>
                                 <div className="flex justify-between items-center">
@@ -36,14 +70,13 @@ const Community = () => {
                                     <div className="text-gray-500 text-sm mt-2 text-right">
                                         <p>{post.username}</p>
                                         <p>{post.time}</p>
-                                        <p>{post.upvote} 点赞</p>
+                                        <button onClick={() => handleLikeClick(post.id)}>{post.upvote} 点赞</button>
                                     </div>
                                 </div>
                             </div>
-                        </a>
                     ))}
                 </div>
-                <Pagination className="text-center mt-4 mb-16" showQuickJumper defaultPageSize={pageSize} total={PostContent.length} onChange={onChange}/>
+                <Pagination className="text-center mt-4 mb-16" showQuickJumper defaultPageSize={pageSize} total={postInfos.length} onChange={onChange}/>
             </div>
         </>
     )
